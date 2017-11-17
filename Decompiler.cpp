962b1da63e20d991ffa781176feb9329895e2d30
nihesh <nihesh16059@iiitd.ac.in>
2017-11-17 19:08:06 +0530

    updated

--- Decompiler.cpp
+++ Decompiler.cpp
@@ -866,10 +866,6 @@ string addsParser(vector<string> instruction){
   translatedcommand = targetTransform + " = "+sourceTransform1 +" + " + sourceTransform2 + ";"+ "\n"+"compareRegister = "+sourceTransform1 +" + "+ sourceTransform2+";";
   return translatedcommand;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 05f23a7eb8ce377638fd619c61552e81882e53e2
 void InitialiseCommands(command_map &listx){
     listx.emplace("add",&addParser);
     listx.emplace("sub",&subParser);
@@ -879,10 +875,6 @@ void InitialiseCommands(command_map &listx){
     listx.emplace("swi",&swiHandler);
     listx.emplace("adds",&addsParser);
     listx.emplace("subs",&subsParser);
-<<<<<<< HEAD
-=======
-
->>>>>>> 05f23a7eb8ce377638fd619c61552e81882e53e2
 }
 
 string decompileSequentialInstruction(vector<string> instruction,command_map listx){
